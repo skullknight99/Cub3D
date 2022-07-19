30d5b2fac995e8927cd1a80444a9141d3cfbe7a1
Achraf Maghous <acmaghou@e3r12p14.1337.ma>
2022-07-19 09:31:33 +0100

    fixed rotation matrix

--- src/rotation.c
+++ src/rotation.c
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 13:20:44 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/07/15 13:20:45 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/07/19 09:29:35 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	left_rotation(t_cub *cub, double old_dir_x, double old_plane_x)
 {
-	cub->plr.dir.x = cub->plr.dir.x * cos(-ROT_SPD)
+	cub->plr.dir.x = cub->plr.dir.x * cos(ROT_SPD)
 		- cub->plr.dir.y * sin(-ROT_SPD);
 	cub->plr.dir.y = old_dir_x * sin(-ROT_SPD)
-		+ cub->plr.dir.y * cos(-ROT_SPD);
-	cub->ray.plane.x = cub->ray.plane.x * cos(-ROT_SPD)
+		+ cub->plr.dir.y * cos(ROT_SPD);
+	cub->ray.plane.x = cub->ray.plane.x * cos(ROT_SPD)
 		- cub->ray.plane.y * sin(-ROT_SPD);
 	cub->ray.plane.y = old_plane_x * sin(-ROT_SPD)
-		+ cub->ray.plane.y * cos(-ROT_SPD);
+		+ cub->ray.plane.y * cos(ROT_SPD);
 }
 
 void	right_rotation(t_cub *cub, double old_dir_x, double old_plane_x)
